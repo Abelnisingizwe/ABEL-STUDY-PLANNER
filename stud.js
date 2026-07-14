9f23175b3043359374894895b502296910de36fc
Abelnisingizwe <abelnisingizwe1@gmail.com>
2026-07-14 10:34:08 +0200

    Update stud.js

--- stud.js
+++ stud.js
@@ -19,7 +19,9 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
-  ref
+  ref,
+  uploadBytes,
+  getDownloadURL
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 // ================= USER =================
 let currentUser = null;
